3b16d2e99becffa8aaa263764d65f77408de5cec
YaySushi <ducklingdood@gmail.com>
2026-05-12 00:40:23 -0400

    format server.js

--- server/server.js
+++ server/server.js
@@ -5,78 +5,86 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 
-app.get('/getSong', (req, res) => {
-	const yt_api_key = process.env.YOUTUBE_API_KEY;
-	const video_id = req.query.video_id;
-	const url = "https://www.googleapis.com/youtube/v3/videos?id=" + video_id + "&key=" + yt_api_key + "&fields=items(snippet(title))&part=snippet";
+app.get('/getSong', async (req, res) => {
+  try {
+    const youtubeApiKey = process.env.YOUTUBE_API_KEY;
+    const videoId = req.query.video_id;
+    const youtubeSearchUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${youtubeApiKey}&fields=items(snippet(title))&part=snippet`;
 
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			var video_name = "";
-			if (data["items"].length > 0)
-				video_name = data["items"][0]["snippet"]["title"];
-			video_name = cleanVideoName(video_name);
+    const response = await fetch(youtubeSearchUrl);
+    const data = await response.json();
 
-			// authenticate using `Client credentials` authorization flow
-			// https://developer.spotify.com/documentation/web-api/concepts/authorization
-			const auth_url = "https://accounts.spotify.com/api/token"
-			const client_id = process.env.SPOTIFY_CLIENT_ID;
-			const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-			const form = {
-				'grant_type': 'client_credentials'
-			}
-			var formBody = [];
-			for (var property in form) {
-				var encodedKey = encodeURIComponent(property);
-				var encodedValue = encodeURIComponent(form[property]);
-				formBody.push(encodedKey + "=" + encodedValue);
-			}
-			formBody = formBody.join("&");
-			options = {
-				method: 'POST',
-				body: formBody,
-				headers: {
-					'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret).toString('base64'),
-					'Content-Type': 'application/x-www-form-urlencoded'
-				},
-			};
+    let videoName = "";
+    if (data["items"].length > 0) {
+      videoName = data["items"][0]["snippet"]["title"];
+      videoName = cleanVideoName(videoName);
+    }
+    console.log(videoName);
 
-			fetch(auth_url, options)
-				.then(response => response.json())
-				.then(data => {
-					access_token = data["access_token"]
-					// get song:
-					const url2 = `https://api.spotify.com/v1/search?q= ${video_name} &type=track`;
-					options = {
-						method: 'GET',
-						headers: {
-							'Authorization': 'Bearer ' + access_token
-						}
-					};
+    // authenticate:
+    const spotifyAuthUrl = "https://accounts.spotify.com/api/token";
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+    const form = {
+      'grant_type': 'client_credentials'
+    };
 
-					fetch(url2, options)
-						.then(response2 => response2.json())
-						.then(data => {
-							tracks = data["tracks"]["items"];
-							if (tracks)
-								return res.send({ body: tracks[0] })
-							return res.status(404).send('Not Found');
-						}).catch(error => { console.error('Error:', error);; });
-				}).catch(error => { console.error('Error:', error); });
-		}).catch(error => { console.error('Error:', error); });
+    let formBody = [];
+    for (let property in form) {
+      const encodedKey = encodeURIComponent(property);
+      const encodedValue = encodeURIComponent(form[property]);
+      formBody.push(`${encodedKey}=${encodedValue}`);
+    }
+    formBody = formBody.join("&");
+
+    const options = {
+      method: 'POST',
+      body: formBody,
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+    };
+
+    const authResponse = await fetch(spotifyAuthUrl, options);
+    const authData = await authResponse.json();
+    const accessToken = authData["access_token"];
+
+    // get song:
+    const spotifySearchUrl = `https://api.spotify.com/v1/search?q=${videoName}&type=track`;
+    const searchOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    };
+
+    const searchResponse = await fetch(spotifySearchUrl, searchOptions);
+    const searchData = await searchResponse.json();
+
+    const tracks = searchData["tracks"]["items"];
+    if (tracks && tracks.length > 0) {
+      return res.send({
+        body: tracks[0]
+      });
+    }
+    return res.status(404).send('Not Found');
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(port, () => {
-	console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
 
-function cleanVideoName(videoname) {
-	// remove anything in brackets (i.e. "(Official Video)") since it messes
-	// up with Spotify's search. also remove dashes.
-	videoname = videoname.replace('-', "");
-	videoname = videoname.replace('"', "");
-	videoname = videoname.replace(/\([\s\S]*\)/g, "");
-	videoname = videoname.replace(/\[[\s\S]*\]/g, "");
-	return videoname;
+function cleanVideoName(videoName) {
+  //remove anything in brackets (i.e. "(Official Video)") since it messes
+  //up with Spotify's search. also remove dashes.
+  videoName = videoName.replace('-', "");
+  videoName = videoName.replace('"', "");
+  videoName = videoName.replace(/\([\s\S]*\)/g, "");
+  videoName = videoName.replace(/\[[\s\S]*\]/g, "");
+  return videoName;
 }
